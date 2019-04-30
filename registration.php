a2664f9c2f5694e77a631f0c9c428668ce8204f6
sunmuffin42 <sunkulp.ananth@gmail.com>
2019-04-30 17:20:56 -0500

    sunmuffin42

--- registration.php
+++ registration.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 
+if (!isset($_SESSION["user"])) {
 $passwds = file("passwd.txt");
 $user = $_POST["userName"];
 $users = array();
@@ -22,8 +24,7 @@ else {
     $response = "Thank you for registering";
     fclose($these_pass);
 }
-session_start();
-$_SESSION["user"] = TRUE;
+$_SESSION["user"] = $user;
 print <<<INIT
 <!DOCTYPE HTML>
 <html>
@@ -50,7 +51,38 @@ print <<<INIT
 </body>
 </html>
 INIT;
+}
 
+else {
+    $user = $_SESSION["user"];
+    $response = "You are already logged in as $user";
+    print <<<INIT
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Food Facts</title>
+	<meta charset="utf-8">
+	<link rel = 'stylesheet' type = 'text/css' href = './foodstrap.css'>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+	<script type="text/javascript" src ="./foodfacts.js"></script>
+</head>
+<body>
+	<header>
+        <a href="./foodstrap.html">
+            <h1>Foodstrap</h1>
+        </a>
+    </header>
+    <nav>
+        <a href="./browse.html" class="navlink" id="link1">Browse Recipes</a>
+        <a href="./submit.html" class="navlink" id="link2">Submit Recipes</a>
+        <a href="./login.html" class="navlink" id="link3">Log in</a>
+        <a href="./contact.html" class="navlink" id="link4">Contact Us</a>
+    </nav>
+    <p>$response</p>
+</body>
+</html>
+INIT;
+}
 
 
 
