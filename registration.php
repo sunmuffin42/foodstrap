c2544cdfd7fd85ed9a3cf0c44764c37cffc60aec
sunmuffin42 <sunkulp.ananth@gmail.com>
2019-04-30 17:13:38 -0500

    session

--- registration.php
+++ registration.php
@@ -22,7 +22,36 @@ else {
     $response = "Thank you for registering";
     fclose($these_pass);
 }
+session_start();
 $_SESSION["user"] = TRUE;
-echo $response;
+print <<<INIT
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Food Facts</title>
+	<meta charset="utf-8">
+	<link rel = 'stylesheet' type = 'text/css' href = './foodstrap.css'>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+	<script type="text/javascript" src ="./foodfacts.js"></script>
+</head>
+<body>
+	<header>
+        <a href="./foodstrap.html">
+            <h1>Foodstrap</h1>
+        </a>
+    </header>
+    <nav>
+        <a href="./browse.html" class="navlink" id="link1">Browse Recipes</a>
+        <a href="./submit.html" class="navlink" id="link2">Submit Recipes</a>
+        <a href="./login.html" class="navlink" id="link3">Log in</a>
+        <a href="./contact.html" class="navlink" id="link4">Contact Us</a>
+    </nav>
+    <p>$response</p>
+</body>
+</html>
+INIT;
+
+
+
 
 ?>
