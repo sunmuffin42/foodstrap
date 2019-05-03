f0b5190f1bd7e6e9c09cd7bb1fc7facfae1f4b11
sunmuffin42 <sunkulp.ananth@gmail.com>
2019-05-02 19:25:28 -0500

    submit_recipe thanks page

--- submit_recipe.php
+++ submit_recipe.php
@@ -96,7 +96,34 @@ INIT;
         $recipe = $_POST["recipe"];
         $recipe = preg_replace('/\n/', ',', $recipe);
         mysqli_query($connect, "insert into $table (user, dish, ingr1, ingr2, ingr3, ingr4, ingr5, recipe) values ($user, $dish, $ingr1, $ingr2, $ingr3, $ingr4, $ingr5, $recipe);");
-
+        mysqli_close($connect);
+        $response = "Thank you for submitting your recipe!";
+        print <<<INIT
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+            <title>Submit a recipe</title>
+            <meta charset='utf-8'>
+            <link rel = 'stylesheet' type = 'text/css' href = './foodstrap.css'>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+            <script type='text/javascript' src ='./foodfacts.js'></script>
+        </head>
+        <body>
+            <header>
+                <a href='./foodstrap.html'>
+                    <h1>Foodstrap</h1>
+                </a>
+            </header>
+            <nav>
+                <a href='./browse.html' class='navlink' id='link1'>Browse Recipes</a>
+                <a href='./submit.html' class='navlink' id='link2'>Submit Recipes</a>
+                <a href='./login.html' class='navlink' id='link3'>Log in</a>
+                <a href='./contact.html' class='navlink' id='link4'>Contact Us</a>
+            </nav>
+            <p>$response</p>
+        </body>
+        </html>
+INIT;
     }
 }
 
